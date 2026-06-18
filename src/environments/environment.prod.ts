4d2e3324a7ab977bb8b14ffac196e9195ca22878
carlosormeno <carlos.ormeno@outlook.es>
2026-06-18 12:36:04 -0500

    feat(prod): agregar build y deploy para ambiente prod

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  api: {
+    baseUrl: 'http://44.238.218.85:5511',
+    rutaSaa: '/login'
+  },
+  bffSecret: 'thaqhiri123',
+  mapboxAccessToken: '${MAPBOX_ACCESS_TOKEN}',
+  mapaMiliSegundosRefresco: 10000
+};
